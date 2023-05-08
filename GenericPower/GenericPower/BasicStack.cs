34f2e71fb94b0e4a6c2efabdc1953bf0fcd6a9cc
YuriiYusko <yura.yusko.y@gmail.com>
2023-05-08 19:17:19 +0200

    V2

--- GenericPower/GenericPower/BasicStack.cs
+++ GenericPower/GenericPower/BasicStack.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenericPower
+﻿namespace GenericPower
 {
-    internal class BasicStack
+    internal class BasicStack<T>
     {
+        private readonly T[] _items;
+
+        private int _currentIndex = -1;
+
+        public BasicStack() => _items = new T[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(T item) => _items[++_currentIndex] = item;
+
+        public T Pop() => _items[_currentIndex--];
     }
 }
--- GenericPower/GenericPower/Program.cs
+++ GenericPower/GenericPower/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GenericPower;
+
+var stack = new BasicStack<double>();
+var stackString = new BasicStack<String>();
+
+stack.Push(4.5);
+stack.Push(43);
+stack.Push(333.6);
+
+stackString.Push("Yurii");
+stackString.Push("Yusko");
+stackString.Push("Test");
+
+while(stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+}
+Console.WriteLine();
+
+while (stackString.Count > 0)
+{
+    Console.WriteLine($"Item: {stackString.Pop()}");
+}
+Console.WriteLine();
